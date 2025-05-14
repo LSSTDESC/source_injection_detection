5722610077ea24909cf6a1a29e859bfdf9b5bf40
shenmingfu <shenming.fu.astro@gmail.com>
2025-05-14 06:48:47 +0000

    added files

---
+++ SL_AGN/run.sh
@@ -0,0 +1,6 @@
+setup lsst_distrib
+
+#============================
+python set_stamp.py
+
+#tar -cvzf fig.tgz fig
---
+++ SL_AGN/set_stamp.py
@@ -0,0 +1,55 @@
+from lib.tools import *
+from lib.stamp import *
+
+
+
+#============================
+SYSTEM_INDEX = 1
+
+TIME_INDEX_LIST = [
+0,
+]
+
+CALEXP_STAMP_FILENAME_LIST = ["%s/system_%d_%d.fits"%(FIG_FOLDER, SYSTEM_INDEX, i) for i in TIME_INDEX_LIST]
+COADD_STAMP_FILENAME = "%s/system_%d_coadd.fits"%(FIG_FOLDER, SYSTEM_INDEX)
+
+MAG_LIST = [
+15.,
+]
+
+#----------------------------
+newline("Start!")
+
+print("SYSTEM_INDEX: ", SYSTEM_INDEX)
+print("TIME_INDEX_LIST: ", TIME_INDEX_LIST)
+
+print()
+
+
+#============================
+newline("get_single_stamp")
+get_single_stamp(SYSTEM_INDEX, TIME_INDEX_LIST[0])
+
+#----------------------------
+newline("get_coadd_stamp")
+get_coadd_stamp(SYSTEM_INDEX)
+
+#----------------------------
+newline("get_diff_stamp")
+get_diff_stamp(SYSTEM_INDEX, TIME_INDEX_LIST[0])
+
+#----------------------------
+newline("check_flux_diff")
+check_flux_diff(SYSTEM_INDEX)
+
+#----------------------------
+newline("add_wcs")
+filename = "%s/system_%d_%d.fits"%(FIG_FOLDER, SYSTEM_INDEX, TIME_INDEX_LIST[0])
+add_wcs(filename)
+
+filename = "%s/system_%d_coadd.fits"%(FIG_FOLDER, SYSTEM_INDEX)
+add_wcs(filename)
+
+
+#============================
+newline("End!")
