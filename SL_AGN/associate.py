b58720d5773c6e5fd70817ec9356e74d98e13b17
shenmingfu <shenming.fu.astro@gmail.com>
2025-05-14 17:30:26 +0000

    updated files

--- SL_AGN/associate.py
+++ SL_AGN/associate.py
@@ -1,1 +1,8 @@
-#associateDiaSources 
+from lib.tools import *
+from lib.imdiff import *
+
+
+#======================================
+diaSources = load_pickle("diaSources")
+difference_image = load_pickle("difference_image")
+newDiaObjects = get_object(diaSource, difference_image)
