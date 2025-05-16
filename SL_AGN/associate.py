4af0c8616b03fc9324993c7fa2aabe3057f0bb80
shenmingfu <shenming.fu.astro@gmail.com>
2025-05-16 00:08:16 +0000

    updated files

--- SL_AGN/associate.py
+++ SL_AGN/associate.py
@@ -12,9 +12,11 @@ difference_image = load_pickle("difference_image")
 
 #----------------------------
 newline("get_object")
-#newDiaObjects = get_object(diaSources, difference_image, BAND)
-#save_pickle("newDiaObjects", newDiaObjects)
+newDiaObjects = get_object(diaSources, difference_image, BAND)
+save_pickle("newDiaObjects", newDiaObjects)
 
-newDiaObjects = load_pickle("newDiaObjects")
-newDiaObjects2 = get_object(diaSources, difference_image, BAND, diaObjects=newDiaObjects)
-save_pickle("newDiaObjects2", newDiaObjects2)
+#newDiaObjects = load_pickle("newDiaObjects")
+#newDiaObjects2 = get_object(diaSources, difference_image, BAND, diaObjects=newDiaObjects)
+#save_pickle("newDiaObjects2", newDiaObjects2)
+
+#----------------------------
--- SL_AGN/run.sh
+++ SL_AGN/run.sh
@@ -1,3 +1,6 @@
+# Running on the Rubin Science Platform
+
+#============================
 setup lsst_distrib
 
 
@@ -15,6 +18,15 @@ setup lsst_distrib
 
 #apdb-cli create-sql "sqlite:///apdb.db" apdb_config.py
 python associate.py
+
+#============================
+#python sqlite3_to_csv.py apdb.db DiaObject DiaObject.csv
+#python sqlite3_to_csv.py apdb.db DiaSource DiaSource.csv
+#python sqlite3_to_csv.py apdb.db SSObject SSObject.csv
+#python sqlite3_to_csv.py apdb.db metadata metadata.csv
+#python sqlite3_to_csv.py apdb.db DiaForcedSource DiaForcedSource.csv
+#python sqlite3_to_csv.py apdb.db DiaObject_To_Object_Match DiaObject_To_Object_Match.csv
+#rename.ul .csv _empty.csv *.csv
 
 #============================
 #tar -cvzf fig.tgz fig
