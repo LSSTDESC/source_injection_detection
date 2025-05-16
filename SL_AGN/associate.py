219601e1a566092314405e76def2bccb08ff82a7
shenmingfu <shenming.fu.astro@gmail.com>
2025-05-16 06:09:31 +0000

    updated files, tested successfully

--- SL_AGN/associate.py
+++ SL_AGN/associate.py
@@ -12,8 +12,10 @@ difference_image = load_pickle("difference_image")
 
 #----------------------------
 newline("get_object")
-newDiaObjects = get_object(diaSources, difference_image, BAND)
-save_pickle("newDiaObjects", newDiaObjects)
+updatedDiaObjects, associatedDiaSources, diaObjectCat = get_object(diaSources, difference_image, BAND)
+save_pickle("updatedDiaObjects", updatedDiaObjects)
+save_pickle("associatedDiaSources", associatedDiaSources)
+save_pickle("diaObjectCat", diaObjectCat)
 
 #newDiaObjects = load_pickle("newDiaObjects")
 #newDiaObjects2 = get_object(diaSources, difference_image, BAND, diaObjects=newDiaObjects)
