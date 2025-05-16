f04914a481049b31d879ba1334d7dc4da8ca7520
shenmingfu <shenming.fu.astro@gmail.com>
2025-05-16 06:04:25 +0000

    updated files, tested successfully

--- SL_AGN/run.sh
+++ SL_AGN/run.sh
@@ -1,4 +1,5 @@
 # Running on the Rubin Science Platform
+# Using v28.0.1
 
 #============================
 setup lsst_distrib
@@ -17,16 +18,15 @@ setup lsst_distrib
 #python check_completeness.py
 
 #apdb-cli create-sql "sqlite:///apdb.db" apdb_config.py
+#python sqlite3_to_csv.py apdb.db DiaObject DiaObject_empty.csv
+#python sqlite3_to_csv.py apdb.db DiaSource DiaSource_empty.csv
 python associate.py
 
 #============================
-#python sqlite3_to_csv.py apdb.db DiaObject DiaObject.csv
-#python sqlite3_to_csv.py apdb.db DiaSource DiaSource.csv
 #python sqlite3_to_csv.py apdb.db SSObject SSObject.csv
 #python sqlite3_to_csv.py apdb.db metadata metadata.csv
 #python sqlite3_to_csv.py apdb.db DiaForcedSource DiaForcedSource.csv
 #python sqlite3_to_csv.py apdb.db DiaObject_To_Object_Match DiaObject_To_Object_Match.csv
-#rename.ul .csv _empty.csv *.csv
 
 #============================
 #tar -cvzf fig.tgz fig
